9bc988c4a2b97978cc04344f0ad828584c8646a3
arora <bhupesh104a@gmail.com>
2019-03-21 03:19:56 +0530

    To pass lint and test for time being

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- *
- * @format
- */
-
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import * as RNLocalize from "react-native-localize";
-import I18n from "./helpers/i18n";
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your  to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends Component<{}> {
+import I18n from "./helpers/i18n";
+
+interface State {
+  message: string;
+}
+export default class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { message: "" };
+  }
   public render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text>{I18n.t("hello")}</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>{I18n.t("ping")}</Text>
       </View>
     );
   }
@@ -37,15 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
   }
 });
