1ba402487a5dc7ae582cf21a0b78cbcfdc6f6ead
bhupessh <bhupesh104a@gmail.com>
2019-03-27 16:33:07 +0530

    Test cases with axios-mock-adapter( for mocking network request)

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,14 +1,46 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import { shallow } from "enzyme";
 import React from "react";
 import App from "../src/App";
-describe("App", () => {
-  describe("checking intial states", () => {
-    const wrapper = shallow(<App />);
-    it("check intial state message", () => {
-      expect(wrapper.state("message")).toEqual("");
-    });
-    it("check intial state errorText", () => {
-      expect(wrapper.state("errorText")).toEqual("");
-    });
+
+
+describe("checking intial states", () => {
+  const wrapper = shallow(<App />);
+  it("check intial state message", () => {
+    expect(wrapper.state("message")).toEqual("");
+  });
+  it("check intial state errorText", () => {
+    expect(wrapper.state("errorText")).toEqual("");
+  });
+});
+
+describe("check ping with success", () => {
+  const mockData = { message: "System" };
+
+  beforeEach(() => {
+    const mock = new MockAdapter(axios);
+    mock.onGet("localhost/ping").reply(200, mockData);
+  });
+  it("check state message", () => {
+    const wrapper = shallow<App>(<App />);
+    wrapper.instance().onPressPing();
+    setTimeout(() => {
+      expect(wrapper.state("message")).toEqual("System");
+    }, 1000);
+  });
+});
+
+describe("check ping request error", () => {
+  beforeEach(() => {
+    const mock = new MockAdapter(axios);
+    mock.onGet("localhost/ping").networkError();
+  });
+  it("check state message", () => {
+    const wrapper = shallow<App>(<App />);
+    wrapper.instance().onPressPing();
+    setTimeout(() => {
+      expect(wrapper.state("errorText")).toEqual("Network Error");
+    }, 1000);
   });
 });
--- setup.js
+++ setup.js
@@ -1,8 +1,14 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+//import * as config from './__mocks__/react-native-config';
 jest.mock("react-native-localize", () => {
     return {
         getLocales: () =>  ({languageTag: "en-US", isRTL: false, countryCode: "US", languageCode: "en"})
+    }
+  });
+  jest.mock("react-native-config", () => {
+    return {
+        Config: {API_ENDPOINT_URL: 'localhost'}
     }
   });
 Enzyme.configure({ adapter: new Adapter() })
